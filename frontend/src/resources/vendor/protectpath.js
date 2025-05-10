491d94a7e08855dcfcbd7966870128ab2b8a3187
Barecha Ashenafi <berechaashenafi@gmail.com>
2025-05-11 01:19:24 +0300

    update to original

--- frontend/src/resources/vendor/protectpath.js
+++ frontend/src/resources/vendor/protectpath.js
@@ -1,46 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Translation from "../translations/lang.json";
 
 function ProtectPathVendor(props) {
-  const [isChecking, setIsChecking] = useState(true);
-  const [vendor, setVendor] = useState(null);
+  const [vendors, setVendors] = useState([]);
+  let Cmp = props.Cmp;
   const navigate = useNavigate();
-  const Cmp = props.Cmp;
-
   useEffect(() => {
-    const vendorInfo = localStorage.getItem('vendor-info');
-    
-    if (!vendorInfo) {
-      navigate("/vendor/login");
-    } else {
-      const vendorData = JSON.parse(vendorInfo); // Assuming vendor info is stored as a JSON string
-      setVendor(vendorData);
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8000/api/vendor/vendorstatus"
+        );
+        const data = await response.json();
+        setVendors(data.users);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-      // Check vendor status and navigate accordingly
-      switch (vendorData.status) {
-        case "UnVerified":
-          navigate("/vendor/vendor-info/");
-          break;
-        case "Pending":
-          navigate("/vendor/underreview/");
-          break;
-        case "Rejected":
-          navigate("/vendor/vendor-info/");
-          break;
-        case "Suspended":
-          navigate("/vendor/suspend/");
-          break;
-        default:
-          setIsChecking(false); // Proceed to render the component if status is valid
+    if (!localStorage.getItem("vendor-info")) {
+      navigate("/vendor/login");
+    } else if (localStorage.getItem("vendor-info")) {
+      if (vendors.status === "UnVerified") {
+        navigate("/vendor/vendor-info/");
+      } else if (vendors.status === "Pending") {
+        navigate("/vendor/underreview/");
+      } else if (vendors.status === "Rejected") {
+        navigate("/vendor/vendor-info/");
+      } else if (vendors.status === "Suspended") {
+        navigate("/vendor/suspend/");
       }
     }
-  }, [navigate]);
-
-  if (isChecking || !vendor) {
-    return null; // or a loading spinner
-  }
-
+  }, []);
   return (
     <div>
       <Cmp />
