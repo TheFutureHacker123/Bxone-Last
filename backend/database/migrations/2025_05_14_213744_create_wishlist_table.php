b183c1b72ef44c11f50fb0191a39c9ec6d95deba
Barecha Ashenafi <berechaashenafi@gmail.com>
2025-05-16 17:56:34 +0300

    updated

---
+++ backend/database/migrations/2025_05_14_213744_create_wishlist_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wishlist', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('product_id'); // Changed to unsignedInteger
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->foreign('product_id')->references('product_id')->on('product')->onDelete('cascade');
+
+            $table->unique(['user_id', 'product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wishlist');
+    }
+};
